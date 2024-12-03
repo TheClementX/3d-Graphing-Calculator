619bcc0dfb131a9666851f2446ed0c80adab9c8c
TheClementX <georgesanders87i4@gmail.com>
2024-12-02 23:07:38 -0600

    good ui/ux

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import MatrixFunctions as mf
 import Graph 
 import function 
 import math
-
+from Button import *
 
 def onAppStart(app):
     # graph app variables
@@ -45,10 +45,26 @@ def onAppStart(app):
     
     #mode 
     app.funcMode = False
-    app.insertMode = True
+    app.insertMode = False
     app.error = False
     app.scaleMode = False
+    app.helpMode = True
 
+    #buttons
+    app.scaleButton = Button(150, 50, 'Scale Mode', 20,)
+    app.insertButton = Button(175, 50, 'Insert Mode', 20)
+    app.functionButton = Button(175,50,'Function Mode', 20)
+    app.zoomInButton = Button(125,50, 'Zoom +', 15)
+    app.zoomOutButton = Button(125, 50, 'Zoom -', 15)
+    app.helpButton = Button(75,50,'Help', 20)
+    app.rotateButton = Button(150,50, 'Rotate', 20)
+    app.resetButton = Button(150,50,'Reset', 20)
+
+    #movement buttons
+    app.upButton = Button(50,50,'up', 15)
+    app.downButton = Button(50,50,'down', 15)
+    app.leftButton = Button(50,50,'left', 15)
+    app.rightButton = Button(50,50,'right', 15)
     #circles are drawing in the correct place spin scaling and lines next
 
     #apply necessary transformations to each element of the graph not including the function
@@ -150,50 +166,93 @@ def drawFunction(app):
         drawCircle(transformedPoint[0], transformedPoint[1], 4, fill='purple')
 
 def redrawAll(app):
-
     drawUI(app)
 
 def drawUI(app):
-    if app.funcMode:
+    app.helpButton.drawButton(50, 950)
+    if app.helpMode:
+        app.functionButton.visible = False
+        app.scaleButton.visible = False
+        app.insertButton.visible = False
+        app.zoomInButton.visible = False
+        app.zoomOutButton.visible = False
+
+        #draw relevant information on screen 
+        drawRect(50,50,900, app.height/4, fill = None, border ='black', borderWidth = 5)
+        drawLabel('General Instructions: ', 165, 75, size = 20, bold = True)
+        drawLabel('Click help to enter/exit help mode', app.width/2, 75, size = 20)
+        drawLabel('This calculator uses four modes: scale, insert, function, and help mode ', app.width/2, 125, size = 20)
+        drawLabel('In scale mode you can scale the graph and adjust precision using number of grid lines', app.width/2, 175, size = 20)
+        drawLabel('In function mode you can view the function', app.width/2, 225, size = 20)
+        drawLabel('In insert mode you can set the function to be graphed using the keyboard', app.width/2, 275, size = 20)
+
+        drawRect(50,350 , 900, app.height/4, fill = None, border ='black', borderWidth = 5)
+        drawLabel('Scale Mode Instructions: ', 185, 375, size = 20, bold = True)
+        drawLabel('to select the field to be changed press x,y,z or n on the keyboard', app.width/2, 425, size = 20)
+        drawLabel('then type in the change you want to be made', app.width/2, 475, size = 20)
+        drawLabel('press the "a" key to apply the changes to the graph ', app.width/2, 525, size = 20)
+        drawLabel('alternatively press the buttons to change graph properties by +/-2', app.width/2, 575, size = 20)
+        
+        drawRect(50, 650 , 900, app.height/4 , fill = None, border ='black', borderWidth = 5)
+        drawLabel('Function Mode Instructions: ', 200, 675, size =20, bold = True )
+        drawLabel('In function mode you can view the function entered in insert mode ', app.width/2, 725, size = 20)
+        drawLabel('press the zoom + or zoom - button to change zoom', app.width/2, 775, size = 20)
+        drawLabel('press the rotating button to stop/start rotation', app.width/2, 825, size = 20)
+        drawLabel('use w/s, a/d, and t/g or arrow buttons to rotate the graph', app.width/2, 875, size = 20)
+
+
+
+    elif app.funcMode:
+        app.functionButton.visible = False
+        app.insertButton.visible = True
+        app.scaleButton.visible = True
+        app.zoomInButton.visible = True
+        app.zoomOutButton.visible = True
+
         drawLabel('Function Mode', app.width/2, 50, size = 60, italic = True)
         drawFunction(app)
         drawGraph(app)
 
         # draw all relevent labels
-        drawLabel(f'function: {app.function.string_function}',(app.width/2),950, size = 20)
+        drawLabel(f'function: {app.function.string_function}',(app.width/2),100, size = 20)
         drawLabel(f'X len:  {str(app.graph.xScope)}', 50, 10)
         drawLabel(f'Y len:  {str(app.graph.yScope)}', 50, 30)
         drawLabel(f'Z len:  {str(app.graph.zScope)}', 50, 50)
         drawLabel(f'zoom: {app.scale}', 920, 10, size = 15)
         drawLabel(f'next zoom: {app.scaleString}', 920, 30, size = 15)
-    elif app.insertMode:
-        drawLabel('Insert Mode', app.width/2, 50, size = 60, italic = True)
-        drawLabel(f'f(x, y) = {app.function.string_function}', app.width/2 , app.height/2 - 50, size=60)
-        #general keybinds
-        drawLabel('General Key Binds: ', app.width/3 , 620, size = 20)
-        drawLabel('w/s: rotate around y axis', app.width/3 , 640, size = 15)
-        drawLabel('a/d: rotate arounnd z axis', app.width/3 , 660, size = 15)
-        drawLabel('t/g: rotate around x axis', app.width/3 , 680, size = 15)
-        drawLabel('r: toggle rotating', app.width/3 , 700, size = 15)
-        drawLabel('u: reset graph position', app.width/3 , 720, size = 15)
-        drawLabel('+: zoom in', app.width/3 , 740, size = 15)
-        drawLabel('-: zoom out', app.width/3 , 760, size = 15)
 
-        #insert keybinds
-        drawLabel('Insert Mode Instructions: ', app.width * 2/4, app.height/2 + 20, size = 20)
-        drawLabel('enter any function in terms of x and y', app.width * 2/4 , app.height/2 + 40, size = 20)
-        drawLabel('use () to surround variables', app.width * 2/4 , app.height/2 + 60, size = 20)
-        
-        #scale mode keybinds
-        drawLabel('Scale Mode Instructions: ', app.width * 2/3, 620, size = 20)
-        drawLabel('press x,y,z,n to select scope', app.width* 2/3, 640, size = 15)
-        drawLabel('enter desired scope value', app.width * 2/3, 660, size = 15)
-        drawLabel('press \'a\' to apply changes', app.width*2/3, 680, size = 15)
-        drawLabel('press \'g\' to toggle grid lines', app.width*2/3, 700, size = 15)
-        drawLabel('press \'b\' to toggle outer box', app.width*2/3, 720, size = 15)
+        #draw Buttons 
+        app.scaleButton.drawButton(200, 950)
+        app.insertButton.drawButton(400, 950)
+        app.zoomInButton.drawButton(920, 80)
+        app.zoomOutButton.drawButton(920, 140)
+        app.rotateButton.drawButton(920, 200)
+        app.resetButton.drawButton(920, 260)
+
+        #draw movement buttons
+        app.upButton.drawButton(875, 875)
+        app.downButton.drawButton(875, 975)
+        app.rightButton.drawButton(950, 925)
+        app.leftButton.drawButton(800, 925)
+
+    elif app.insertMode:
+        app.insertButton.visible = False
+        app.scaleButton.visible = True
+        app.functionButton.visible = True
+
+        drawLabel('3D Graphing Calculator', app.width/2, 50, size = 60, bold = True, italic = True)
+        drawLabel('Insert Mode', app.width/2, 150, size = 60, italic = True)
+        drawLabel(f'f(x, y) = {app.function.string_function}', app.width/2 , app.height/2 - 50, size=60)
+        app.scaleButton.drawButton(200, 950)
+        app.functionButton.drawButton(400, 950)
 
     elif app.scaleMode:
+        app.scaleButton.visible = False
+        app.insertButton.visible = True
+        app.functionButton.visible = True
+
         drawLabel('Scale Mode', app.width/2, 50, size = 60, italic = True)
+        drawLabel('press I to return to insert mode', app.width/2, 100, size = 20, bold = True)
         drawLabel(f'X scale:  {str(app.xScope)}', app.width/2, app.height/2 - 40, size=30)
         drawLabel(f'Y scale:  {str(app.yScope)}', app.width/2, app.height/2, size=30)
         drawLabel(f'Z scale:  {str(app.zScope)}', app.width/2, app.height/2 + 40, size=30)
@@ -201,14 +260,14 @@ def drawUI(app):
 
         drawLabel(f'Grid Lines On: {app.drawGrid}', app.width/2, app.height/2-80, size = 30)
         drawLabel(f'Box On: {app.drawBox}', app.width/2, app.height/2-120, size = 30)
+        
+        app.insertButton.drawButton(200, 950)
+        app.functionButton.drawButton(400, 950)
     elif app.error:
         drawLabel('invalid function entered', 800, 200)
 
-
 #time based step functions / event loop equivalent
 def onStep(app):
-#     print(app.selectedScope, app.xScope, app.yScope, app.zScope)
-#     print('graph', app.graph.xScope, app.graph.yScope, app.graph.zScope)
     if app.funcMode:
         takeStep(app)
  
@@ -226,8 +285,34 @@ def takeStep(app):
 
 
 
-
-    #mf.projectPoint(app.matrices.)
+def onMousePress(app, mouseX, mouseY):
+    if app.zoomInButton.getPressed(mouseX, mouseY):
+        if app.scale < 50: app.scale += 2
+    if app.zoomOutButton.getPressed(mouseX, mouseY):
+        if app.scale >0: app.scale -= 2
+    if app.helpButton.getPressed(mouseX, mouseY):
+        app.helpMode = not app.helpMode
+        if not app.funcMode and not app.scaleMode and not app.insertMode: app.insertMode = True
+    if app.scaleButton.getPressed(mouseX, mouseY):
+        app.scaleMode = True
+        app.funcMode = False
+        app.insertMode = False
+    if app.insertButton.getPressed(mouseX, mouseY):
+        app.insertMode = True
+        app.funcMode = False
+        app.scaleMode = False
+    if app.functionButton.getPressed(mouseX, mouseY):
+        app.funcMode = True
+        app.insertMode = False
+        app.scaleMode = False 
+    if app.upButton.getPressed(mouseX, mouseY): app.matrices.tx -= math.pi/12
+    if app.downButton.getPressed(mouseX, mouseY): app.matrices.tx += math.pi/12
+    if app.leftButton.getPressed(mouseX, mouseY): app.matrices.ty += math.pi/12
+    if app.rightButton.getPressed(mouseX, mouseY): app.matrices.ty -= math.pi/12
+    if app.resetButton.getPressed(mouseX, mouseY):
+        app.matrices.ty = 0 
+        app.matrices.tz = 0 
+        app.matrices.tx = 0
 
 def onKeyPress(app, key):
 
